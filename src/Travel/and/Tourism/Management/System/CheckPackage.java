53903c680f869e3762d178d6831c17a7dd2b188e
NabajitUtsab <noboutsab@gmail.com>
2024-05-11 16:13:27 +0600

    Modified by Nabajit Dey

--- src/Travel/and/Tourism/Management/System/CheckPackage.java
+++ src/Travel/and/Tourism/Management/System/CheckPackage.java
@@ -8,6 +8,25 @@ public class CheckPackage extends JFrame {
     CheckPackage(){
         setBounds(250,100,900,600);
 
+        JPanel panel1=createPackage();
+        JPanel panel2=createPackage();
+        JPanel panel3=createPackage();
+
+
+
+        JTabbedPane tabbedPane=new JTabbedPane();
+        tabbedPane.addTab("Package 1",null,panel1);
+        tabbedPane.addTab("Package 2",null,panel2);
+        tabbedPane.addTab("Package 3",null,panel3);
+
+
+        add(tabbedPane);
+
+        setVisible(true);
+    }
+
+
+    public JPanel createPackage(){
         JPanel panel1=new JPanel();
         panel1.setLayout(null);
         panel1.setBackground(Color.WHITE);
@@ -43,14 +62,50 @@ public class CheckPackage extends JFrame {
         buffetLabel.setForeground(Color.blue);
         panel1.add(buffetLabel);
 
+        JLabel drinksLabel=new JLabel("Welcome Drinks on Arrival");
+        drinksLabel.setBounds(30,260,300,30);
+        drinksLabel.setFont(new Font("Tahoma",Font.BOLD,20));
+        drinksLabel.setForeground(Color.red);
+        panel1.add(drinksLabel);
 
+        JLabel cruiseLabel=new JLabel("Full day 3 Island Cruise");
+        cruiseLabel.setBounds(30,310,300,30);
+        cruiseLabel.setFont(new Font("Tahoma",Font.BOLD,20));
+        cruiseLabel.setForeground(Color.blue);
+        panel1.add(cruiseLabel);
 
-        JTabbedPane tabbedPane=new JTabbedPane();
-        tabbedPane.addTab("Package 1",null,panel1);
+        JLabel guideLabel=new JLabel("English Speaking Guide");
+        guideLabel.setBounds(30,360,300,30);
+        guideLabel.setFont(new Font("Tahoma",Font.BOLD,20));
+        guideLabel.setForeground(Color.red);
+        panel1.add(guideLabel);
 
-        add(tabbedPane);
+        JLabel bookLabel=new JLabel("Book Now !!");
+        bookLabel.setBounds(100,410,300,30);
+        bookLabel.setFont(new Font("Tahoma",Font.BOLD,20));
+        bookLabel.setForeground(Color.black);
+        panel1.add(bookLabel);
 
-        setVisible(true);
+        JLabel seasonLabel=new JLabel("Summer Special");
+        seasonLabel.setBounds(100,460,300,30);
+        seasonLabel.setFont(new Font("Tahoma",Font.BOLD,25));
+        seasonLabel.setForeground(Color.MAGENTA);
+        panel1.add(seasonLabel);
+
+        JLabel costLabel=new JLabel("Per person BDT 40,000/- ");
+        costLabel.setBounds(500,460,500,30);
+        costLabel.setFont(new Font("Tahoma",Font.BOLD,25));
+        costLabel.setForeground(Color.CYAN);
+        panel1.add(costLabel);
+
+        ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("icons/package1.jpg"));
+        Image image= icon.getImage().getScaledInstance(500,300,Image.SCALE_DEFAULT);
+        ImageIcon imageIcon=new ImageIcon(image);
+        JLabel imageLabel=new JLabel(imageIcon);
+        imageLabel.setBounds(350,20,500,300);
+        panel1.add(imageLabel);
+
+        return panel1;
     }
 
     public static void main(String[] args) {
